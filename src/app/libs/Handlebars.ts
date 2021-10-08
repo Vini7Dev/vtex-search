02c2e572975683efbc6152568af91dcde1d04e97
Vinícius Gabriel <68403363+Vinicius7Dev@users.noreply.github.com>
2021-10-08 11:47:44 -0300

    Add handlebars to email template

---
+++ src/app/libs/Handlebars.ts
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import handlebars from 'handlebars';
+
+// Definindo o tipo das variáveis para o template
+interface ITemplateVariables {
+    [key: string]: string | number;
+}
+
+// Dados para realizar o parse do template
+interface IParseMailRequest {
+    file: string;
+    variables: ITemplateVariables[];
+}
+
+class Handlebars {
+    public async parse({
+        file,
+        variables,
+    }: IParseMailRequest): Promise<string> {
+        // Lendo o arquivo de template
+        const template = await fs.promises.readFile(file, {
+            encoding: 'utf-8',
+        });
+
+        // Compilando o arquivo de template com o handlebars
+        const parseTemplate = handlebars.compile(template);
+
+        // Retornando o template preenchido com as variáveis
+        return parseTemplate(variables);
+    }
+}
+
+export default Handlebars;
