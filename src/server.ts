a2b666c8e423e594fc277523a019687dc4a4f7e2
Vinícius Gabriel <68403363+Vinicius7Dev@users.noreply.github.com>
2021-10-08 10:59:14 -0300

    Add bull board

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import 'dotenv/config';
 import express from 'express';
+import { createBullBoard } from 'bull-board';
+import { BullAdapter } from 'bull-board/bullAdapter';
+import { BullMQAdapter } from 'bull-board/bullMQAdapter';
 
+import Queue from './app/libs/Queue';
 import mainRoutes from './app/routes';
 
 // Criando o servidor
@@ -11,6 +15,14 @@ app.use(express.json());
 
 // Aplicando as rotas no servidor
 app.use(mainRoutes);
+
+// Configurando o bull board para as queues
+const { router: queuesRouter } = createBullBoard(
+    Queue.queues.map(queue => new BullAdapter(queue.bull)),
+);
+
+// Declarando a rota para acessar o bull board
+app.use('/admin/queues', queuesRouter);
 
 // Iniciando o servidor
 app.listen(3333, () => {
